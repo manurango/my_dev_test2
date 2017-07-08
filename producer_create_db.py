ddcab87ed89f3e079ae27f120e5ee002c42e99fc
manurango <emmanuelmarango9@gmail.com>
2017-07-08 20:05:55 +0300

    Update producer_create_db.py

--- producer_create_db.py
+++ producer_create_db.py
@@ -4,19 +4,20 @@ from stompest.config import StompConfig
 from stompest.sync import Stomp
 import json
 #read the csv file values on column 5
+#the csv file is created by running create_csv.py
 
 fields = ['id', 'num5']
 df = pd.read_csv('tests.csv', skipinitialspace=True, usecols=fields)
 x = df.name.values
 q = str(x)
-#check to see if values C an G exist on column 5
+#check to see if values C or  G exist on column 5
 #if yes create test db and store row values
 if 'C' or 'G' in q:
     conn = sqlite3.connect("test.sqlite")
     conn.execute("CREATE TABLE if not exists Data (id TEXT, col5 TEXT)")
     df = pd.read_csv("tests.csv")
     df.to_sql("Data", conn, if_exists='append', index=False)
-#check to see if values C and G exist on column 5
+#check to see if only  values C and G exist on column 5
 #if yes create connect to apacheMq and store values
 elif 'C' and 'G'in q:
     CONFIG = StompConfig('tcp://localhost:61613')
